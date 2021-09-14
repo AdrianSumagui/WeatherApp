064dbde8ad742f9128ee507a8adf38eeeb15e532
AdrianDEV <adriansumagui@outlook.com>
2021-09-14 11:47:56 +0200

    Definición de la estructura del archivo Page.jsx e importación de sus respectivos componentes y hoja de estilos.

--- src/components/Page/Page.jsx
+++ src/components/Page/Page.jsx
@@ -1,11 +1,40 @@
 import React, { Fragment } from 'react';
 
+// Importo la hoja de estilos.
+import styles from './Page.module.css';
+
+// Importo los componentes.
 import Header from '../Header';
+import Form from '../Form';
+import Error from '../Error';
+import Loader from '../Loader';
+import Forecast from '../Forecast';
 
 const Page = () => {
     return (
         <Fragment>
+
             <Header />
+
+            <div className= {styles.box}>
+
+            {/* Formulario para introducir la localidad deseada. */}
+
+            <Form/>
+
+            {/* Error */}
+
+            <Error/>
+            {/* Loader (Animación de carga.) */}
+
+            <Loader/>
+
+            {/* Forecast (Pronóstico del tiempo.) */}
+
+            <Forecast/>
+
+            </div>
+
         </Fragment>
     );
 };
